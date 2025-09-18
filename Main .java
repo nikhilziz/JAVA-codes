9bb93f5f909e2013d6bfa3db79457c205b0ee95a
nikhilziz <nikhil.kumar.cs28@iilm.edu>
2025-09-18 09:44:20 +0530

    Create Main .java

---
+++ Main .java
@@ -0,0 +1,11 @@
+class Main {
+    // static block
+   
+
+    public static void main(String[] args) {
+        System.out.println("from main");
+    }
+     static {
+        System.out.println("Static block initialized.");
+    }
+}
